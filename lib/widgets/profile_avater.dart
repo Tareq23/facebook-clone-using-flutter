6a4461815638011350dfca77bd861c78d9b3a0d4
Tareq23 <mdtarequlislam634@gmail.com>
2022-07-29 00:40:53 +0600

    user stories

--- lib/widgets/profile_avater.dart
+++ lib/widgets/profile_avater.dart
@@ -6,8 +6,9 @@ class ProfileAvater extends StatelessWidget {
 
   final String imageUrl;
   final bool isActive;
+  final bool hasBorder;
 
-  const ProfileAvater({super.key, required this.imageUrl, this.isActive = false});
+  const ProfileAvater({super.key, required this.imageUrl, this.isActive = false, this.hasBorder = false});
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +16,11 @@ class ProfileAvater extends StatelessWidget {
       children:[
         CircleAvatar(
           radius: 20.0,
-          backgroundColor: Colors.grey[200],
-          backgroundImage: CachedNetworkImageProvider(imageUrl),
+          child: CircleAvatar(
+            radius: hasBorder ? 17.0 : 20,
+            backgroundColor: Colors.grey[200],
+            backgroundImage: CachedNetworkImageProvider(imageUrl),
+          ),
         ),
         if(isActive)Positioned(
           bottom: 0,
---
+++ lib/widgets/stories.dart
@@ -0,0 +1,119 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebookclone/config/palette.dart';
+import 'package:facebookclone/widgets/profile_avater.dart';
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class Stories extends StatelessWidget {
+  final User currentUser;
+  final List<Story> stories;
+
+  const Stories({super.key, required this.currentUser, required this.stories});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      color: Colors.white,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(
+          vertical: 8.0,
+          horizontal: 10.0,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + stories.length,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: _StoryCard(
+                isAddStory: true,
+                currentUser: currentUser,
+                story: stories[0],
+              ),
+            );
+          }
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: _StoryCard(
+              isAddStory: false,
+              currentUser: currentUser,
+              story: stories[index - 1],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  bool isAddStory;
+  final User currentUser;
+  final Story story;
+
+  _StoryCard(
+      {super.key,
+      this.isAddStory = false,
+      required this.currentUser,
+      required this.story});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: CachedNetworkImage(
+            height: double.infinity,
+            width: 110.0,
+            fit: BoxFit.cover,
+            imageUrl: isAddStory ? currentUser.imageUrl : story!.imageUrl,
+          ),
+        ),
+        Container(
+          width: 110.0,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: Palette.storyGradient,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        Positioned(
+          top: 8,
+          left: 8,
+          child: isAddStory
+              ? Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: const BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () => print("add story"),
+                    icon: Icon(Icons.add),
+                    iconSize: 30,
+                    color: Palette.facebookBlue,
+                  ),
+                )
+              : ProfileAvater(
+                  imageUrl: story.user.imageUrl,
+                  hasBorder: story.isViewed,
+                ),
+        ),
+        Positioned(
+          left: 8.0,
+          right: 8.0,
+          bottom: 8.0,
+          child: Text(
+            isAddStory ? "Add to Story" : story.user.name,
+            style: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          )
+        )
+      ],
+    );
+  }
+}
