10376a908ad1916e00f3e096d5e44425a32fe908
Tareq23 <mdtarequlislam634@gmail.com>
2022-07-28 10:33:01 +0600

    circle button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:facebookclone/config/palette.dart';
+import 'package:facebookclone/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,16 +15,28 @@ class HomeScreen extends StatelessWidget {
           SliverAppBar(
             systemOverlayStyle: SystemUiOverlayStyle.light,
             backgroundColor: Colors.white,
-            title: Text("facebook",style: TextStyle(
-                color: Palette.facebookBlue,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: -1.2
-            ),
+            title: Text(
+              "facebook",
+              style: TextStyle(
+                  color: Palette.facebookBlue,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.5),
             ),
             centerTitle: false,
             floating: true,
-
+            actions: [
+              CircleButton(
+                icon: Icons.search,
+                iconSize: 30.0,
+                onPressed: () => print("search"),
+              ),
+              CircleButton(
+                  icon: MdiIcons.facebookMessenger,
+                  iconSize: 30.0,
+                  onPressed: () => print("Messengar"),
+              )
+            ],
           )
         ],
       ),
